f57bd6bf70de9500d6df8445f227547411ea7107
gersin <gersonfernandesribeiro48@gmail.com>
2024-09-05 19:49:37 -0300

    first commit

---
+++ src/main/java/com/workspacepi/apiquoteflow/ApiQuoteFlowApplication.java
@@ -0,0 +1,13 @@
+package com.workspacepi.apiquoteflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQuoteFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiQuoteFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workspacepi/apiquoteflow/ApiQuoteFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspacepi.apiquoteflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiQuoteFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
