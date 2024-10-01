a94e1a1dbd38391059ca7d5dfc849824ee525d41
gersin <gersonfernandesribeiro48@gmail.com>
2024-10-01 20:54:54 -0300

    update errors

--- src/main/java/com/workspacepi/apiquoteflow/adapters/jdbc/enderecos/EnderecosJDBCRepository.java
+++ src/main/java/com/workspacepi/apiquoteflow/adapters/jdbc/enderecos/EnderecosJDBCRepository.java
@@ -13,11 +13,13 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-import com.workspacepi.apiquoteflow.adapters.http.cotacoes.error.EnderecoErrorHandler;
+//import com.workspacepi.apiquoteflow.adapters.http.cotacoes.error.EnderecoErrorHandler;
 import com.workspacepi.apiquoteflow.domain.enderecos.*;
 
 import java.util.List;
 import java.util.UUID;
+
+import static com.workspacepi.apiquoteflow.adapters.jdbc.enderecos.EnderecosSqlExpressions.sqlSelectAllEnderecos;
 
 
 // Nosso repositório que define os nossos métodos de query e de crud usando o JDBC
@@ -35,7 +37,7 @@ public class EnderecosJDBCRepository implements EnderecoRepository {
 
     // Logger cuida do envio das nossas exceptions específicas ao invés das exceptions padrões
 
- private static final Logger LOGGER = LoggerFactory.getLogger(EnderecoErrorHandler.class);
+//    private static final Logger LOGGER = LoggerFactory.getLogger(EnderecoErrorHandler.class);
 
 
 //  Função da RowMapper para aproveitamento de código
@@ -53,7 +55,7 @@ public class EnderecosJDBCRepository implements EnderecoRepository {
                 UUID id_empresa_endereco = UUID.fromString(rs.getString("id_empresa_endereco"));
                 UUID id_endereco = UUID.fromString(rs.getString("id_endereco"));
 
-                return new Endereco();
+                return new Endereco(bairro_endereco, cep_endereco, complemento_endereco, localidade_endereco, logradouro_endereco, numero_endereco, uf_endereco, id_empresa_endereco, id_endereco);
         };
     }
 
@@ -61,15 +63,15 @@ public class EnderecosJDBCRepository implements EnderecoRepository {
 
     private MapSqlParameterSource parameterSource(Endereco endereco){
         MapSqlParameterSource params = new MapSqlParameterSource();
-        params.addValue("bairro_endereco", endereco.getBairro_endereco);
-        params.addValue("cep_endereco", endereco.getCep_endereco);
-        params.addValue("complemento_endereco", endereco.getComplemento_endereco);
-        params.addValue("localidade_endereco", endereco.getLocalidade_endereco);
-        params.addValue("logradouro_endereco", endereco.getLogradouro_endereco);
-        params.addValue("numero_endereco", endereco.getNumero_endereco);
-        params.addValue("uf_endereco", endereco.getUf_endereco);
-        params.addValue("id_empresa_endereco", endereco.getid_empresa_endereco);
-        params.addValue("id_endereco", endereco.getId_endereco);
+        params.addValue("bairro_endereco", endereco.getBairro_endereco());
+        params.addValue("cep_endereco", endereco.getCep_endereco());
+        params.addValue("complemento_endereco", endereco.getComplemento_endereco());
+        params.addValue("localidade_endereco", endereco.getLocalidade_endereco());
+        params.addValue("logradouro_endereco", endereco.getLogradouro_endereco());
+        params.addValue("numero_endereco", endereco.getNumero_endereco());
+        params.addValue("uf_endereco", endereco.getUf_endereco());
+        params.addValue("id_empresa_endereco", endereco.getId_empresa_endereco());
+        params.addValue("id_endereco", endereco.getId_endereco());
         return params;
     }
 
@@ -81,7 +83,7 @@ public class EnderecosJDBCRepository implements EnderecoRepository {
             return enderecos;
 
         } catch (Exception e) {
-            LOGGER.error("Houve um erro ao consultar os endereços: " + e.getMessage());
+//            LOGGER.error("Houve um erro ao consultar os endereços: " + e.getMessage());
             throw e;
         }
     }
